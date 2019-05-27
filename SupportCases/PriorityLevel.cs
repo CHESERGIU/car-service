827f91d5eca47f5f45216d374658f59f972564f9
CHESERGIU <sergiu.chereches@gmail.com>
2019-05-27 22:54:15 +0300

    class PriorityLevel

---
+++ SupportCases/PriorityLevel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportCases
+{
+    public class PriorityLevel
+    {
+    }
+
+    public enum Low
+    {
+    }
+
+    public enum Medium
+    {
+    }
+
+    public enum Important
+    {
+    }
+
+    public enum Critical
+    {
+    }
+
+    
+}
