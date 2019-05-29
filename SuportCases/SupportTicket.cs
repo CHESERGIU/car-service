efd8735ee9cda9a904ac9836ad19c694cd4f8951
CHESERGIU <sergiu.chereches@gmail.com>
2019-05-30 00:56:37 +0300

    refactor class SupportTicket

--- SuportCases/SupportTicket.cs
+++ SuportCases/SupportTicket.cs
@@ -21,27 +21,42 @@ namespace SuportCases
             System.Console.Read();
         }
         public static int Partition(SupportTicket[] tickets, int start, int end)
+        {            
+            var partitionIndex = Index(tickets, start, end);
+            Swap(tickets, end, partitionIndex);
+            return partitionIndex;
+        }
+
+        private static void Swap(SupportTicket[] tickets, int end, int partitionIndex)
+        {
+            var temp1 = tickets[partitionIndex];
+            tickets[partitionIndex] = tickets[end];
+            tickets[end] = temp1;
+        }
+
+        private static int Index(SupportTicket[] tickets, int start, int end)
         {
             var pivot = (int)tickets[end].Priority;
             var partitionIndex = start;
             for (var i = start; i < end; i++)
             {
                 if ((int)tickets[i].Priority > pivot) continue;
-                var temp = tickets[partitionIndex];
-                tickets[partitionIndex] = tickets[i];
-                tickets[i] = temp;
+                Swap(tickets, partitionIndex, i);
                 partitionIndex++;
             }
-            var temp1 = tickets[partitionIndex];
-            tickets[partitionIndex] = tickets[end];
-            tickets[end] = temp1;
             return partitionIndex;
         }
+              
         public static SupportTicket[] ReadSupportTickets()
         {
             var ticketsNumber = System.Convert.ToInt32(System.Console.ReadLine());
             var result = new SupportTicket[ticketsNumber];
+            GetTickets(ticketsNumber, result);
+            return result;
+        }
 
+        static void GetTickets(int ticketsNumber, SupportTicket[] result)
+        {
             for (var i = 0; i < ticketsNumber; i++)
             {
                 var ticketData = System.Console.ReadLine()?.Split('-');
@@ -51,9 +66,8 @@ namespace SuportCases
                     result[i] = new SupportTicket(id, ticketData[1].Trim(), GetPriorityLevel(ticketData[2]));
                 }
             }
-
-            return result;
         }
+
         public static void Quick3Sort(SupportTicket[] tickets, int start, int end)
         {
             while (true)
