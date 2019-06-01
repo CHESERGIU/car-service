9b077bdf96ad7620be911b57989da326e179ab4a
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-01 15:45:30 +0300

    Main read data and output

--- SuportCases/Pocket.cs
+++ SuportCases/Pocket.cs
@@ -5,9 +5,9 @@ namespace CarService
 {
     public class Pocket
     {
-        private readonly string CarNumber;
-        private readonly string Problem;
-        private readonly WaitingTime Priority;
+         public readonly string CarNumber;
+         public readonly string Problem;
+         public readonly WaitingTime Priority;
 
         public Pocket(string id, string problem, WaitingTime priority)
         {
@@ -29,25 +29,5 @@ namespace CarService
             }
             return WaitingTime.Scheduled;
         }
-
-        public static void Print(Pocket[] tickets)
-        {
-            for (var i = tickets.Length - 1; i >= 0; i--)
-                Console.WriteLine(tickets[i].CarNumber + " - " + tickets[i].Problem + " - " + tickets[i].Priority);
-            Console.Read();
-        }
-
-        public static Pocket[] Input()
-        {
-            var ticket1 = new Pocket("CJ01ABC", "Direction check", WaitingTime.Delegated);
-            var ticket2 = new Pocket("CJ02DEF", "Lights not working", DeadLine);
-            var ticket3 = new Pocket("CJ03GHI ", "Battery drain", Scheduled);
-            var ticket4 = new Pocket("CJ04JKL  ", "Engine break critical", WaitingTime.Urgent);
-
-            Pocket[] tickets = { ticket1, ticket2, ticket3, ticket4 };
-            
-            return tickets;
-        }
-
     }
 }
--- SuportCases/Program.cs
+++ SuportCases/Program.cs
@@ -6,7 +6,16 @@ namespace CarService
     {
         public static void Main()
         {
-            Pocket.Print(Pocket.Input());                    
+            Pocket ticket1 = new Pocket("CJ01ABC", "Direction check", WaitingTime.Delegated);
+            Pocket ticket2 = new Pocket("CJ02DEF", "Lights not working", WaitingTime.DeadLine);
+            Pocket ticket3 = new Pocket("CJ03GHI ", "Battery drain", WaitingTime.Scheduled);
+            Pocket ticket4 = new Pocket("CJ04JKL  ", "Engine break critical", WaitingTime.Urgent);
+
+            Pocket[] tickets = { ticket1, ticket2, ticket3, ticket4 };
+
+            for (var i = tickets.Length - 1; i >= 0; i--)
+                Console.WriteLine(tickets[i].CarNumber + " - " + tickets[i].Problem + " - " + tickets[i].Priority);
+            Console.Read();
         }
     }
 }
