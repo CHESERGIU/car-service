c91f47d6890d8303dec4271c85a2507ae0aebc43
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-01 14:55:24 +0300

    add tickets to tests

--- SuportCases.Tests/Program.Tests.cs
+++ SuportCases.Tests/Program.Tests.cs
@@ -9,10 +9,14 @@ namespace CarService.Tests
         public void When_tickets_for_support_request_are_1_must_return_1()
         {
             var ticket1 = new Pocket("CJ01ABC", "Direction check", WaitingTime.Delegated);
+            var ticket2 = new Pocket("CJ02DEF", "Lights not working", WaitingTime.DeadLine);
+            var ticket3 = new Pocket("CJ03GHI ", "Battery drain", WaitingTime.Scheduled);
+            var ticket4 = new Pocket("CJ04JKL  ", "Engine break critical", WaitingTime.Urgent);
 
-            Pocket[] ticket = { ticket1 };
+            Pocket[] ticket = { ticket1, ticket2, ticket3, ticket4 };
             
-            Pocket[] resultTickets = { ticket1 };
+            
+            Pocket[] resultTickets = { ticket1, ticket2, ticket3, ticket4 };
 
             Assert.Equal(resultTickets, resultTickets);
         }
--- SuportCases.Tests/WaitingTime.Tests.cs
+++ SuportCases.Tests/WaitingTime.Tests.cs
@@ -8,10 +8,13 @@ namespace CarService.Tests
         public void When_tickets_for_support_request_are_1_must_return_1()
         {
             var ticket1 = WaitingTime.Delegated;
+            var ticket2 = WaitingTime.DeadLine;
+            var ticket3 = WaitingTime.Scheduled;
+            var ticket4 = WaitingTime.Urgent;
 
-            WaitingTime[] ticket = { ticket1 };
+            WaitingTime[] ticket = { ticket1, ticket2, ticket3, ticket4 };
 
-            WaitingTime[] resultTickets = {  ticket1 };
+            WaitingTime[] resultTickets = { ticket1, ticket2, ticket3, ticket4 };
 
             Assert.Equal(resultTickets, resultTickets);
         }
