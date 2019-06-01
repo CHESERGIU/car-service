18293499ca94aa9e3afbf3b949a194d15e30d594
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-01 14:24:59 +0300

    Initial Encapsulation

--- SuportCases/Pocket.cs
+++ SuportCases/Pocket.cs
@@ -5,23 +5,48 @@ namespace CarService
 {
     public class Pocket
     {
-        public readonly WaitingTime Priority;
+        private readonly string CarNumber;
+        private readonly string Problem;
+        private readonly WaitingTime Priority;
 
-        public Pocket(string id, string problem, WaitingTime priority) => this.Priority = priority;
+        public Pocket(string id, string problem, WaitingTime priority)
+        {
+            this.CarNumber = id;
+            this.Problem = problem;
+            this.Priority = priority;
+        }
 
-        public static WaitingTime GetWaitingTime(string priority)
+        private static WaitingTime GetWaitingTime(string priority)
         {
             switch (priority.ToLower().Trim())
             {
-                case "Urgent":
+                case "urgent":
                     return WaitingTime.Urgent;
-                case "DeadLine":
+                case "deadline":
                     return WaitingTime.DeadLine;
-                case "Delegated":
+                case "delegated":
                     return WaitingTime.Delegated;
             }
-
             return WaitingTime.Scheduled;
+        }
+
+        public static void Print(Pocket[] tickets)
+        {
+            for (var i = tickets.Length - 1; i >= 0; i--)
+                Console.WriteLine(tickets[i].CarNumber + " - " + tickets[i].Problem + " - " + tickets[i].Priority);
+            Console.Read();
+        }
+
+        public static Pocket[] Input()
+        {
+            var ticket1 = new Pocket("CJ01ABC", "Direction check", WaitingTime.Delegated);
+            var ticket2 = new Pocket("CJ02DEF", "Lights not working", DeadLine);
+            var ticket3 = new Pocket("CJ03GHI ", "Battery drain", Scheduled);
+            var ticket4 = new Pocket("CJ04JKL  ", "Engine break critical", WaitingTime.Urgent);
+
+            Pocket[] ticket = { ticket1, ticket2, ticket3, ticket4 };
+            
+            return ticket;
         }
 
     }
--- SuportCases/Program.cs
+++ SuportCases/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using static CarService.Pocket;
 
 namespace CarService
 {
     public class Program
     {
-        public static void Main() => Print((Console.ReadLine()?.Split('-')));
-        
-        public static void Print(string[] input)
+        public static void Main()
         {
-            Console.WriteLine("{0} - {1} - {2}", input[0], input[1].Trim(), GetWaitingTime(input[2]));
-            Console.Read();
+            Pocket.Print(Pocket.Input());                    
         }
     }
 }
